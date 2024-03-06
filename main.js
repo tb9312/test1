2c20120ae0061f31e7c6bfda4c56761adf429161
tb9312 <158120200+tb9312@users.noreply.github.com>
2024-03-07 01:02:09 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+document.getElementById("button-reviews").addEventListener("click", function() {
+    var overlay = document.createElement("div");
+    overlay.classList.add("overlay");
+
+    var popup = document.createElement("div");
+    popup.classList.add("popup");
+    var content = document.createElement("p");
+    content.textContent = "câu 1: 1 + 1 = ?";
+    popup.appendChild(content);
+    var content = document.createElement("p");
+    content.textContent = "câu 1: 1 + 1 = ?";
+    popup.appendChild(content);
+    var content = document.createElement("p");
+    content.textContent = "câu 1: 1 + 1 = ?";
+    popup.appendChild(content);
+    var content = document.createElement("p");
+    content.textContent = "câu 1: 1 + 1 = ?";
+    popup.appendChild(content);
+    var content = document.createElement("p");
+    content.textContent = "câu 1: 1 + 1 = ?";
+    popup.appendChild(content);
+    var content = document.createElement("p");
+    content.textContent = "câu 1: 1 + 1 = ?";
+    popup.appendChild(content);
+    var content = document.createElement("p");
+    content.textContent = "câu 1: 1 + 1 = ?";
+    popup.appendChild(content);
+    var content = document.createElement("p");
+    content.textContent = "câu 1: 1 + 1 = ?";
+    popup.appendChild(content);
+    var content = document.createElement("p");
+    content.textContent = "câu 1: 1 + 1 = ?";
+    popup.appendChild(content);
+    var content = document.createElement("p");
+    content.textContent = "câu 1: 1 + 1 = ?";
+    popup.appendChild(content);
+
+    overlay.appendChild(popup);
+    document.body.appendChild(overlay);
+});
